63f40cbdf14a66f8d594e89f968f982a952fb52d
George Jalland <george.jalland@gmail.com>
2024-04-13 16:49:07 +0100

    introduce random offset and rename filename

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, redirect
 from select_file import get_random_video
 
 app = Flask(__name__)
 
-video_directory = '/Users/georg/Videos/Captures/'
-random_video = get_random_video(video_directory)
+VIDEO_DIRECTORY = '/Users/georg/Videos/Captures/'
+app.config['random_video'] = get_random_video(VIDEO_DIRECTORY)
 
 @app.route('/')
 def main():
-    return render_template('index.html', path_to_video=f"/video/{random_video}")
+    vid = app.config['random_video']
+    return render_template('index.html', path_to_video=f"/video/{vid}")
 
 @app.route('/video/<filename>')
 def video(filename):
     # Serve the video file using send_from_directory
-    return send_from_directory(video_directory, filename)
+    return send_from_directory(VIDEO_DIRECTORY, filename)
 
 @app.route('/next_video')
 def next_video():
-    return 
+    next_vid = get_random_video(VIDEO_DIRECTORY, 1)
+    app.config['random_video'] = next_vid
+    return redirect('/')
--- select_file.py
+++ select_file.py
@@ -4,15 +4,21 @@ from random import randrange
 
 today = datetime.now().date()
 
-def get_random_video(directory):
+def get_random_video(directory, offset = 0):
+    offset = offset
     days = 0
     eligible_vids = []
-    while not eligible_vids:
+    while days < 30: # Only go back 30 days to avoid infinite loop
         date = today - timedelta(days)
         eligible_vids = get_videos(directory, date)
+        if eligible_vids:
+            if not offset:
+                eligible_vids = remove_filename_whitespace(directory, eligible_vids)
+                return eligible_vids[randrange(len(eligible_vids))]
+            else:
+                 offset -= 1
         days += 1
-        
-    return eligible_vids[randrange(len(eligible_vids))]
+
 
 def get_videos(directory, date):
         vids = os.listdir(directory)
@@ -25,5 +31,15 @@ def get_videos(directory, date):
 
         return eligible_vids
 
+def remove_filename_whitespace(directory, files):
+    for file_name in files:
+         if " " in file_name:
+            old_path = os.path.join(directory, file_name)
+            new_path = os.path.join(directory, file_name.replace(' ', '_'))
+            os.rename(old_path, new_path)
+        
+    return files
+     
+
 if __name__ == "__main__":
-    print(get_random_video('/Users/georg/Videos/Captures/'))
+    print(get_random_video('/Users/georg/Videos/Captures/', offset=1))
