ca676faa83e4a57547bd707db964595ea0b61702
George Jalland <george.jalland@gmail.com>
2024-04-14 14:02:44 +0100

    refactor as class

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, send_from_directory, redirect
-from select_file import get_random_video
+from flask import Flask, render_template, send_from_directory, redirect, session
+from select_file import VideoManager
 
 app = Flask(__name__)
 
 VIDEO_DIRECTORY = '/Users/georg/Videos/Captures/'
-app.config['random_video'] = get_random_video(VIDEO_DIRECTORY)
+video_manager = VideoManager(VIDEO_DIRECTORY)
 
 @app.route('/')
 def main():
-    vid = app.config['random_video']
+    vid = session.get("video")
+    if vid is None:
+        vid = video_manager.get_latest_video()
     return render_template('index.html', path_to_video=f"/video/{vid}")
 
 @app.route('/video/<filename>')
@@ -18,6 +20,5 @@ def video(filename):
 
 @app.route('/next_video')
 def next_video():
-    next_vid = get_random_video(VIDEO_DIRECTORY, 1)
-    app.config['random_video'] = next_vid
+    session['video'] = video_manager.get_random_video(VIDEO_DIRECTORY, 1)
     return redirect('/')
--- select_file.py
+++ select_file.py
@@ -2,44 +2,44 @@ import os
 from datetime import datetime, timedelta
 from random import randrange
 
-today = datetime.now().date()
+class VideoManager:
+    def __init__(self, directory):
+        self.directory = directory
 
-def get_random_video(directory, offset = 0):
-    offset = offset
-    days = 0
-    eligible_vids = []
-    while days < 30: # Only go back 30 days to avoid infinite loop
-        date = today - timedelta(days)
-        eligible_vids = get_videos(directory, date)
-        if eligible_vids:
-            if not offset:
-                eligible_vids = remove_filename_whitespace(directory, eligible_vids)
-                return eligible_vids[randrange(len(eligible_vids))]
-            else:
-                 offset -= 1
-        days += 1
-
-
-def get_videos(directory, date):
-        vids = os.listdir(directory)
+    def _remove_filename_whitespace(self, files):
+        for file_name in files:
+            if " " in file_name:
+                old_path = os.path.join(self.directory, file_name)
+                new_path = os.path.join(self.directory, file_name.replace(' ', '_'))
+                os.rename(old_path, new_path)
+            
+        return files
+    
+    def get_latest_video(self):
+        return os.listdir(self.directory)[-1]
+    
+    def get_videos(self, date):
+        vids = os.listdir(self.directory)
         eligible_vids = []
         for vid_name in vids:
-            filepath = os.path.join(directory, vid_name)
+            filepath = os.path.join(self.directory, vid_name)
             created_time = datetime.fromtimestamp(os.path.getctime(filepath))
             if created_time.date() == date:
                 eligible_vids.append(vid_name)
 
         return eligible_vids
-
-def remove_filename_whitespace(directory, files):
-    for file_name in files:
-         if " " in file_name:
-            old_path = os.path.join(directory, file_name)
-            new_path = os.path.join(directory, file_name.replace(' ', '_'))
-            os.rename(old_path, new_path)
-        
-    return files
-     
-
-if __name__ == "__main__":
-    print(get_random_video('/Users/georg/Videos/Captures/', offset=1))
+    
+    def get_random_video(self, offset):
+        today = datetime.now().date()
+        days = 0
+        eligible_vids = []
+        while days < 30: # Only go back 30 days to avoid infinite loop
+            date = today - timedelta(days)
+            eligible_vids = self.get_videos(self.directory, date)
+            if eligible_vids:
+                if not offset:
+                    eligible_vids = self._remove_filename_whitespace(self.directory, eligible_vids)
+                    return eligible_vids[randrange(len(eligible_vids))]
+                else:
+                    offset -= 1
+            days += 1
