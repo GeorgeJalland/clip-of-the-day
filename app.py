ceed87ffb51582f07a76dc2a065431e3c9d7b875
George Jalland <george.jalland@gmail.com>
2024-04-14 23:25:34 +0100

    refactor to get files from subdirs

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 from flask import Flask, render_template, send_from_directory, redirect, session
 from urllib.parse import quote
-from select_file import VideoManager
+from video_manager import VideoManager
 
 app = Flask(__name__)
 app.secret_key = 'dc726efe259959c9c259996fa4ef418e'
 
 VIDEO_DIRECTORY = '/Users/georg/OneDrive/Captures/'
-video_manager = VideoManager(VIDEO_DIRECTORY)
+video_manager = VideoManager(VIDEO_DIRECTORY, game="Rocket League", format=".mp4")
 
 @app.route('/')
 def main():
-    print(session.get('video'))
     vid = session.get('video', video_manager.get_latest_video())
-    return render_template('index.html', path_to_video=f"/video/{quote(vid)}")
+    return render_template('index.html', path_to_video=f"/video/{vid['subdir']}/{quote(vid['filename'])}")
 
-@app.route('/video/<filename>')
-def video(filename):
+@app.route('/video/<subdir>/<filename>')
+def video(subdir, filename):
     # Serve the video file using send_from_directory
-    return send_from_directory(VIDEO_DIRECTORY, filename)
+    return send_from_directory(VIDEO_DIRECTORY+subdir, filename)
 
 @app.route('/next-video')
 def next_video():
-    session['video'] = video_manager.get_random_video(1)
+    session['video'] = video_manager.get_random_video(offset=1)
     return redirect('/')
 
 @app.route('/original')
--- video_manager.py
+++ video_manager.py
@@ -3,20 +3,29 @@ from datetime import datetime, timedelta
 from random import randrange
 
 class VideoManager:
-    def __init__(self, directory):
+    def __init__(self, directory, game, format):
         self.directory = directory
+        self.game = game
+        self.format = format
     
     def get_latest_video(self):
-        return os.listdir(self.directory)[-1]
+        # this needs to return sub direcory
+        return sorted(self.get_all_game_videos(), key=lambda x: x['filename'])[-1]
     
-    def get_videos(self, date):
-        vids = os.listdir(self.directory)
+    def get_all_game_videos(self):
+        vids = []
+        for root, _, files in os.walk(self.directory):
+            vids += [{"subdir": os.path.basename(root), "filename":file} for file in files if self.game in file and self.format in file]
+        return vids
+    
+    def get_videos_by_date(self, date):
+        vids = self.get_all_game_videos()
         eligible_vids = []
-        for vid_name in vids:
-            filepath = os.path.join(self.directory, vid_name)
+        for vid in vids:
+            filepath = os.path.join(self.directory, vid['subdir'], vid['filename'])
             created_time = datetime.fromtimestamp(os.path.getctime(filepath))
             if created_time.date() == date:
-                eligible_vids.append(vid_name)
+                eligible_vids.append(vid)
 
         return eligible_vids
     
@@ -26,7 +35,7 @@ class VideoManager:
         eligible_vids = []
         while days < 30: # Only go back 30 days to avoid infinite loop
             date = today - timedelta(days)
-            eligible_vids = self.get_videos(date)
+            eligible_vids = self.get_videos_by_date(date)
             if eligible_vids:
                 if not offset:
                     return eligible_vids[randrange(len(eligible_vids))]
