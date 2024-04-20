0aa9a42f6654224c56ac085d44196bf62e90efcb
George Jalland <george.jalland@gmail.com>
2024-04-20 17:43:16 +0100

    refactor

--- video_manager.py
+++ video_manager.py
@@ -18,14 +18,20 @@ class VideoManager:
     def get_nth_latest_video(self, game, n):
         vids = self.get_all_game_videos(game)
         index = len(vids) - (n % len(vids)) - 1 # loop back to latest video if end of list reached
-        return index, sorted(vids, key=lambda x: x['filename'])[index]
+        return index, vids[index]
+    
+    @return_dict
+    def get_random_video(self, game):
+        vids = self.get_all_game_videos(game)
+        index = randrange(len(vids))
+        return index, vids[index]
     
     @cached(TTLCache(maxsize=10, ttl=600))
     def get_all_game_videos(self, game):
         vids = []
         for root, _, files in os.walk(self.directory):
             vids += [{"subdir": os.path.basename(root), "filename":file} for file in files if game in file and self.format in file]
-        return vids
+        return sorted(vids, key=lambda x: x['filename'])
     
     def get_video_count(self, game):
         return len(self.get_all_game_videos(game))
@@ -40,9 +46,3 @@ class VideoManager:
                 eligible_vids.append(vid)
 
         return eligible_vids
-
-    @return_dict
-    def get_random_video(self, game):
-        vids = self.get_all_game_videos(game)
-        index = randrange(len(vids))
-        return index, vids[index]
