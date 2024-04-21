190e34c111d3af33815eb01eff14b19798866f2c
George Jalland <george.jalland@gmail.com>
2024-04-21 21:23:50 +0100

    type hinting added

--- video_manager.py
+++ video_manager.py
@@ -2,30 +2,31 @@ import os
 from datetime import datetime
 from random import randrange
 from cachetools import cached, TTLCache
+from typing import Dict
 
 class VideoManager:
-    def __init__(self, directory, format):
+    def __init__(self, directory: str, format: str):
         self.directory = directory
         self.format = format
     
-    def get_video_by_index(self, game, index):
+    def get_video_by_index(self, game: str, index: int) -> Dict[int, Dict[str,str]]:
         vids = self.get_all_game_videos(game)
         return {'index': index, 'vid': vids[index % len(vids)]}
     
-    def get_random_video(self, game):
+    def get_random_video(self, game: str) -> Dict[int, Dict[str,str]]:
         return self.get_video_by_index(game, randrange(self.get_video_count(game)))
     
     @cached(TTLCache(maxsize=10, ttl=600))
-    def get_all_game_videos(self, game):
+    def get_all_game_videos(self, game) -> Dict[str,str]:
         vids = []
         for root, _, files in os.walk(self.directory):
             vids += [{"subdir": os.path.basename(root), "filename":file} for file in files if game in file and self.format in file]
         return sorted(vids, key=lambda x: x['filename'], reverse=True) # return list of vids descending by date in filename
     
-    def get_video_count(self, game):
+    def get_video_count(self, game: str) -> int:
         return len(self.get_all_game_videos(game))
     
-    def get_videos_by_date(self, game, date):
+    def get_videos_by_date(self, game: str, date):
         vids = self.get_all_game_videos(game)
         eligible_vids = []
         for vid in vids:
