74b7210e234552c392bf7f46e8c1ed0bf1adaf98
George Jalland <george.jalland@gmail.com>
2024-04-24 21:26:00 +0100

    refactor to use factory

--- app.py
+++ app.py
@@ -2,70 +2,75 @@ from flask import Flask, render_template, send_from_directory, redirect, session
 from urllib.parse import quote
 from video_manager import VideoManager
 import os
-
-app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable track modifications for SQLAlchemy
+from db_models import db
+from config import Config
 
 VIDEO_DIRECTORY = os.getenv('VIDEO_DIRECTORY')
 GAMES = ["Rocket League", "Fortnite"]
 video_manager = VideoManager(VIDEO_DIRECTORY, format=".mp4")
 
-@app.route('/')
-def main():
-    player = session.setdefault('player', '')
-    game = session.setdefault('game', GAMES[0])
-    vid_index, vid = session.setdefault('video', video_manager.get_random_video(game=game, player=player)).values()
-    video_count = video_manager.get_video_count(game, player)
-    players = video_manager.get_all_game_subdirs(game)
+def create_app(config_class=Config):
 
-    return render_template(
-            'index.html', 
-            game=game.upper(), 
-            path_to_video=f"/video/{quote(vid['subdir'])}/{quote(vid['filename'])}", 
-            player=vid['subdir'].upper(), 
-            filedate=vid['filename'][-23:-4],
-            vid_index=video_count - (vid_index % video_count),
-            video_count=video_count,
-            players = players,
-            current_player = player
-        )
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
 
-@app.route('/video/<subdir>/<filename>')
-def video(subdir, filename):
-    return send_from_directory(VIDEO_DIRECTORY+subdir, filename)
+    @app.route('/')
+    def main():
+        player = session.setdefault('player', '')
+        game = session.setdefault('game', GAMES[0])
+        vid_index, vid = session.setdefault('video', video_manager.get_random_video(game=game, player=player)).values()
+        video_count = video_manager.get_video_count(game, player)
+        players = video_manager.get_all_game_subdirs(game)
 
-@app.route('/latest-video')
-def latest_video():
-    session['video'] = video_manager.get_video_by_index(game=session.get('game'), player=session.get('player'), index=0)
-    return redirect('/')
+        return render_template(
+                'index.html', 
+                game=game.upper(), 
+                path_to_video=f"/video/{quote(vid['subdir'])}/{quote(vid['filename'])}", 
+                player=vid['subdir'].upper(), 
+                filedate=vid['filename'][-23:-4],
+                vid_index=video_count - (vid_index % video_count),
+                video_count=video_count,
+                players = players,
+                current_player = player
+            )
 
-@app.route('/random-video')
-def random_video():
-    session.pop('video', None)
-    return redirect('/')
+    @app.route('/video/<subdir>/<filename>')
+    def video(subdir, filename):
+        return send_from_directory(VIDEO_DIRECTORY+subdir, filename)
 
-@app.route('/iterate-video')
-def iterate_video():
-    prev_or_next = request.args.get('iterate')
-    new_index = session.get('video').get('index') + (1 if prev_or_next == 'next' else - 1)
-    session['video']['index'] = new_index
-    session['video'] = video_manager.get_video_by_index(game=session.get('game'), player=session.get('player'), index=new_index)
-    return redirect('/')
+    @app.route('/latest-video')
+    def latest_video():
+        session['video'] = video_manager.get_video_by_index(game=session.get('game'), player=session.get('player'), index=0)
+        return redirect('/')
 
-@app.route('/change-game')
-def change_game():
-    game_index = GAMES.index(session.get('game'))
-    session['game'] = GAMES[(game_index + 1) % len(GAMES)]
-    session.pop('video', None)
-    session.pop('player', None)
-    return redirect('/')
+    @app.route('/random-video')
+    def random_video():
+        session.pop('video', None)
+        return redirect('/')
 
-@app.route('/change-player')
-def change_player():
-    player = request.args.get('player')
-    session['player'] = player
-    session.pop('video')
-    return redirect('/')
+    @app.route('/iterate-video')
+    def iterate_video():
+        prev_or_next = request.args.get('iterate')
+        new_index = session.get('video').get('index') + (1 if prev_or_next == 'next' else - 1)
+        session['video']['index'] = new_index
+        session['video'] = video_manager.get_video_by_index(game=session.get('game'), player=session.get('player'), index=new_index)
+        return redirect('/')
+
+    @app.route('/change-game')
+    def change_game():
+        game_index = GAMES.index(session.get('game'))
+        session['game'] = GAMES[(game_index + 1) % len(GAMES)]
+        session.pop('video', None)
+        session.pop('player', None)
+        return redirect('/')
+
+    @app.route('/change-player')
+    def change_player():
+        player = request.args.get('player')
+        session['player'] = player
+        session.pop('video')
+        return redirect('/')
+    
+    return app
 
