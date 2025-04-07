8b9d3265742cc35b91cbbf8f45ea3413aef1e5bf
George Jalland <george.jalland@gmail.com>
2025-04-07 22:07:11 +0100

    progress bar, pause play button added

--- app/static/components/video.js
+++ app/static/components/video.js
@@ -7,11 +7,18 @@ export class Video {
         this.elements = {
             video: document.getElementById("videoElement"),
             videoSource: document.getElementById("videoSource"),
+            progressBar: document.getElementById("progressBar"),
+            customControls: document.getElementById("customControls"),
+            pauseplayButton: document.getElementById("pauseplayButton"),
         }
+        this.hideControlsTimeout = null;
         this.addListeners()
     }
 
     addListeners() {
+        this.elements.video.addEventListener("timeupdate", () => this.updateProgressBar());
+        this.elements.video.addEventListener("click", () => this.handleClickVideo())
+        this.elements.pauseplayButton.addEventListener("click", () => this.pauseplay())
     }
 
     setState(newState) {
@@ -23,5 +30,40 @@ export class Video {
         this.elements.videoSource.src = videoData.path
         this.elements.video.load()
         this.elements.video.play()
+        this.resetProgressBar()
+    }
+
+    updateProgressBar() {
+        const percent = (this.elements.video.currentTime / this.elements.video.duration) * 100;
+        this.elements.progressBar.style.width = percent + "%";
+    }
+
+    resetProgressBar() {
+        this.elements.progressBar.style.width = "0%";
+    }
+
+    handleClickVideo() {
+        if (!this.elements.video.paused) {
+            this.tempShowControlsAndMeta()
+        }
+    }
+
+    tempShowControlsAndMeta() {
+        this.elements.customControls.classList.add("visible");
+        clearTimeout(this.hideControlsTimeout);
+        this.hideControlsTimeout = setTimeout(() => {
+            this.elements.customControls.classList.remove("visible");
+        }, 2000);
+    }
+
+    pauseplay() {
+        if (this.elements.video.paused) {
+            this.elements.video.play();
+            this.elements.customControls.classList.remove("visible");
+        } else {
+            this.elements.video.pause();
+            clearTimeout(this.hideControlsTimeout);
+            this.elements.customControls.classList.add("visible");
+        }
     }
 }
